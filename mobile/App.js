97eae34a15d224c8298f5ee34da72f647b58fb1b
djaracz <dawid.jaracz@gmail.com>
2018-01-01 21:32:49 +0100

    apollo client init

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,12 @@
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Main } from './components/Main';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-// const client = new ApolloClient({
-  // networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/__PROJECT_ID__'}),
-// });
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://graphql.example.com' }),
+  cache: new InMemoryCache()
+});
 
 export default Main;
