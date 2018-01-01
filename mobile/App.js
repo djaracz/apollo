fc4df01631d3f1e06841f556da39fc179799d13e
djaracz <dawid.jaracz@gmail.com>
2018-01-01 21:18:23 +0100

    mobile packages resolved

--- mobile/App.js
+++ mobile/App.js
@@ -1,23 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Main } from './components/Main';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+// const client = new ApolloClient({
+  // networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/__PROJECT_ID__'}),
+// });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Main;
